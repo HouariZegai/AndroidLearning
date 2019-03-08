e2e272573ce8cccb94f0e892d1a03db34bb965b9
HouariZEGAI <houarizegai14@gmail.com>
2019-03-08 18:08:52 +0100

    Making animation (start/stop timer) - StopWatch

--- StopWatch/app/src/main/java/com/houarizegai/stopwatch/StopWatchActivity.java
+++ StopWatch/app/src/main/java/com/houarizegai/stopwatch/StopWatchActivity.java
@@ -10,12 +10,14 @@ import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.Chronometer;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class StopWatchActivity extends AppCompatActivity {
 
     ImageView icanchor;
     Chronometer timer;
     Button btnStart, btnStop;
+    Animation roundingalone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +29,43 @@ public class StopWatchActivity extends AppCompatActivity {
         btnStart = findViewById(R.id.btnStart);
         btnStop = findViewById(R.id.btnStop);
 
+        // Create optional animation
+        btnStop.setAlpha(0f);
+        btnStop.setTranslationY(-80);
+
+        // Load animation
+        roundingalone = AnimationUtils.loadAnimation(this, R.anim.roundingalone);
+
         // Import font
         Typeface fontMedium = Typeface.createFromAsset(getAssets(), "fonts/MMedium.ttf");
 
         // Customize font
         btnStart.setTypeface(fontMedium);
         btnStop.setTypeface(fontMedium);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Passing animation
+                icanchor.setAnimation(roundingalone);
+                btnStart.animate().alpha(0).setDuration(300).start();
+                btnStop.animate().alpha(1).translationY(0).setDuration(300).start();
+
+                // Start time
+                timer.setBase(SystemClock.elapsedRealtime());
+                timer.start();
+            }
+        });
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timer.stop();
+                // Reset animation
+                icanchor.clearAnimation();
+                btnStart.animate().alpha(1).setDuration(300).start();
+                btnStop.animate().alpha(0).translationY(-80).setDuration(300).start();
+            }
+        });
 
     }
 }
