3e63de7f6c9395f8aa0c5e2cd1447e686203c0dc
HouariZegai <houarizegai14@gmail.com>
2020-05-28 03:32:57 +0100

    Material UI - Making Register View

---
+++ MaterialUI/app/src/main/java/com/example/first/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.first
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
